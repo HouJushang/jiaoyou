2c4dd4f7f58255902255cb7e386b15fc9555c9f2
hou <5466315@qq.com>
2017-01-20 17:46:27 +0800

    chat style

--- src/page/talk/Chat.vue
+++ src/page/talk/Chat.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="chat">
     <TalkNavComponent action="3"></TalkNavComponent>
-    <dl>
+    <dl class="chatList">
       <dd v-for="item in data">
-        <div>
-          <img :src="item.avatar"/>
+        <div class="avatar">
+          <img :src="item.avatar" width="43" height="43"/>
         </div>
-        <div>
+        <div class="chatContent">
           <p>{{item.name}}</p>
           <p>{{item.content}}</p>
         </div>
-        <div>
+        <div class="chatNumber">
           <p>
             {{item.time}}
           </p>
@@ -23,7 +23,46 @@
   </div>
 </template>
 <style lang='sass'>
+  @import "../../assets/comm"
   .chat
+    .chatList
+      background-color: #fff
+      padding: 0 12px
+      dd
+        border-bottom: 1px solid #ececec
+        padding: 10px 0
+        overflow: hidden
+        @include flex()
+        .avatar
+          width: 43px
+          height: 43px
+          @include border-radius(100%)
+          overflow: hidden
+          margin-right: 13px
+          float: left
+        .chatNumber
+          width: 35px
+          p
+            &:first-child
+              font-size: 11px
+              color: #999
+            &:last-child
+              font-size: 14px
+        .chatContent
+          @include flex-grow(1)
+          @include flex()
+          @include align-items(center)
+          @include flex-direction(column)
+          p
+            &:first-child
+              font-size: 16px
+            &:last-child
+              font-size: 14px
+              color: #999999
+
+
+
+
 </style>
 <script>
   import {chat} from '../../data.js'
